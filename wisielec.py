196a8ca252f0de8a540861a67b34432883b3d30f
PiotrK <61495748+pidr98@users.noreply.github.com>
2022-05-28 15:41:40 +0200

    hackton1

--- wisielec.py
+++ wisielec.py
@@ -1,17 +1,18 @@
-
 def get_word():
     word = 'fluster'
     return word.lower()
 
 def game(word):
 
-    word_progress = '_' * len(word)
     guessed = False
     guessed_letters = []
     guessed_words = []
+    word_progress = '_' * len(word)
     tries = 5
+
     print(type(guessed_letters))
     print(type(guessed_words))
+    print(type(word))
     print('--------------Start--------------')
     print('Word to guess: ',word_progress)
 
@@ -21,20 +22,14 @@ def game(word):
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 print('Already guessed this letter', guess)
-
             elif guess not in word:
                 print(f'-{guess}- not in the word')
                 tries -= 1
                 print(f'Tries left: {tries}')
-                guessed_letters.append(guess)
-
             else:
                 print(f'You guessed right!, -{guess}- is in the word!')
-
-                guessed_letters.append(guess)
                 word_to_list = list(word_progress)
                 indexes = [i for i, letter in enumerate(word) if letter == guess]
-
                 for index in indexes:
                     word_to_list[index] = guess
                 word_progress = "".join(word_to_list)
@@ -44,12 +39,10 @@ def game(word):
         elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
                 print("You already guessed the word", guess)
-            elif guess != word:
+            elif guess not in word:
                 print(guess, "is not the word.")
                 tries -= 1
-                guessed_words.append(guess)
                 print(f'Tries left: {tries}')
-
             else:
                 guessed = True
                 word_progress = word
