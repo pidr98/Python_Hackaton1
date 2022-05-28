b264ff1ddcc1bcc3cf8a1562bc8922dbe1d3003d
PiotrK <61495748+pidr98@users.noreply.github.com>
2022-05-28 16:18:25 +0200

    hackton1

--- wisielec.py
+++ wisielec.py
@@ -6,22 +6,21 @@ def game(word):
 
     guessed = False
     guessed_letters = []
-    guessed_words = []
     word_progress = '_' * len(word)
     tries = 5
 
-    print(type(guessed_letters))
-    print(type(guessed_words))
-    print(type(word))
+#    print(type(guessed_letters))
+#    print(type(guessed_words))
+#    print(type(word))
     print('--------------Start--------------')
-    print('Word to guess: ',word_progress)
+    print(f'Word to guess: {word_progress}')
 
     while not guessed and tries > 0:
         guess = input('Input a guess: ').lower()
 
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
-                print('Already guessed this letter', guess)
+                print(f'Already guessed this letter {guess}', )
             elif guess not in word:
                 print(f'-{guess}- not in the word')
                 tries -= 1
@@ -38,7 +37,7 @@ def game(word):
 
         elif len(guess) == len(word) and guess.isalpha():
             if guess not in word:
-                print(guess, "is not the word.")
+                print(f'{guess} is not the word.')
                 tries -= 1
                 print(f'Tries left: {tries}')
             else:
